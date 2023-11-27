4202cef6a230c982f7b7c3e64185ffeed6074bbf
MattZander24 <mattzanderm24mail.com>
2023-11-27 15:56:08 -0300

    Parade v1.3

--- src/ar/edu/unlu/parade/vistaconsola/FuncionesConsola.java
+++ src/ar/edu/unlu/parade/vistaconsola/FuncionesConsola.java
@@ -4,22 +4,12 @@ import java.util.Scanner;
 
 public class FuncionesConsola {
 
-    //?
-    public static void LimpiarPantalla () {
-        try {
-            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
     public static void PulseEnter () {
         System.out.print("Pulse ENTER para continuar");
         Scanner scannerEnter = new Scanner(System.in);
         if (scannerEnter.hasNextLine()) {
             scannerEnter.nextLine();
         }
-        LimpiarPantalla();
         System.out.print("\n");
     }
 }
